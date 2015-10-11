19cacef85fe4d42de28de82371f3e7eca297e5ea
Dan Wahlin <dwahlin@xmlforasp.net>
2015-10-11 00:59:18 -0700

    Initial check-in

---
+++ src/AccountAtAGlance.Repository/Seeding/DatabaseInitializer.cs
@@ -0,0 +1,96 @@
+﻿using AccountAtAGlance.Repository.Helpers;
+using Microsoft.Data.Entity;
+using System;
+using Microsoft.Framework.DependencyInjection;
+using System.Threading.Tasks;
+using Microsoft.Data.Entity.SqlServer;
+using AccountAtAGlance.Repository.Interfaces;
+
+namespace AccountAtAGlance.Repository.Seeding
+{
+    public static class DatabaseInitializer
+    {
+        public static async Task SeedAsync(IServiceProvider serviceProvider)
+        {
+            using (var context = serviceProvider.GetService<AccountAtAGlanceContext>())
+            {
+                var db = context.Database;
+                if (db != null)
+                {
+                    if (await db.EnsureCreatedAsync())
+                    {
+                        await InsertSampleData(serviceProvider);
+                    }
+                }
+                else
+                {
+                    await InsertSampleData(serviceProvider);
+                }
+            }
+        }
+
+        public static async Task InsertSampleData(IServiceProvider serviceProvider)
+        {
+            await Task.Run(async () =>
+            {
+                using (var context = serviceProvider.GetService<AccountAtAGlanceContext>())
+                {
+                    context.Database.ExecuteSqlCommand(@"
+                    CREATE PROCEDURE dbo.DeleteSecuritiesAndExchanges
+
+                    AS
+	                    BEGIN
+	 
+	 		                    BEGIN TRANSACTION
+		                    BEGIN TRY
+			                    DELETE FROM Position;   
+			                    DELETE FROM Stock;
+			                    DELETE FROM MutualFund;
+			                    DELETE FROM Exchange; 
+			                    DELETE FROM MarketIndex;
+			                    COMMIT TRANSACTION
+			                    SELECT 0				
+		                    END TRY
+		                    BEGIN CATCH
+			                    ROLLBACK TRANSACTION
+			                    SELECT -1		
+		                    END CATCH
+	
+	                    END
+                    ");
+
+                    context.Database.ExecuteSqlCommand(@"
+                    CREATE PROCEDURE dbo.DeleteAccounts
+
+                    AS
+	                    BEGIN
+
+		                    BEGIN TRANSACTION
+			                    BEGIN TRY
+				                    DELETE FROM [Order];                                              
+				                    DELETE FROM BrokerageAccount;
+				                    DELETE FROM Customer;					
+				                    COMMIT TRANSACTION
+				                    SELECT 0				
+			                    END TRY
+			                    BEGIN CATCH
+				                    ROLLBACK TRANSACTION
+				                    SELECT -1		
+			                    END CATCH
+	                    END	
+	                ");
+
+                    var sr = serviceProvider.GetService<ISecurityRepository>();
+                    await sr.InsertSecurityDataAsync();
+
+                    var mr = serviceProvider.GetService<IMarketsAndNewsRepository>();
+                    await mr.InsertMarketDataAsync();
+
+                    var ar = serviceProvider.GetService<IAccountRepository>();
+                    await ar.CreateCustomerAsync();
+                    await ar.CreateAccountPositionsAsync();
+                }
+            });
+        }
+    }
+}
---
+++ src/AccountAtAGlance/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace AccountAtAGlance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
